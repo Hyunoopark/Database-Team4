5e1fb09259c8ba6cc8638bfe9423dae94c5773dc
codesmart3 <49859209+codesmart3@users.noreply.github.com>
2019-05-18 16:36:29 +0900

    Update download_apps.sql

--- download_apps.sql
+++ download_apps.sql
@@ -9,14 +9,4 @@ CREATE TABLE `app`.`download_apps` (
 
 INSERT INTO `app`.`download_apps` (`user_id`, `app_id`, `date`, `paid_amount`, `app_name`) VALUES ('23', '1', '2019-05-18', '0', 'Lifeove Bible');
 
-
-
-DROP TRIGGER IF EXISTS `app`.`download_apps_AFTER_INSERT`;
-
-DELIMITER $$
-USE `app`$$
-CREATE DEFINER=`root`@`localhost` TRIGGER `app`.`download_apps_AFTER_INSERT` AFTER INSERT ON `download_apps` FOR EACH ROW
-BEGIN
-INSERT INTO apps SET apps.downloaded_num = apps.downloaded_num + 1;
-END$$
-DELIMITER ;
+       
