2e4d78b5f0898c87e8485d68d13acb79b37fd996
codesmart3 <49859209+codesmart3@users.noreply.github.com>
2019-05-18 19:45:43 +0900

    Update download_apps.sql

--- download_apps.sql
+++ download_apps.sql
@@ -6,7 +6,22 @@ CREATE TABLE `app`.`download_apps` (
   `app_name` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`user_id`, `app_id`));
 
+ALTER TABLE `app`.`download_apps` 
+CHANGE COLUMN `user_id` `email_id` VARCHAR(50) NOT NULL ;
+               
+ALTER TABLE `app`.`download_apps` 
+DROP COLUMN `app_name`;
 
-INSERT INTO `app`.`download_apps` (`user_id`, `app_id`, `date`, `paid_amount`, `app_name`) VALUES ('23', '1', '2019-05-18', '0', 'Lifeove Bible');
-
-       
+ALTER TABLE `app`.`download_apps` 
+ADD COLUMN `is_deleted` TINYINT NULL DEFAULT 0 AFTER `paid_amount`;
+               
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21300333@handong.edu', '1', '2019-05-18', '0', '0');
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21500771@handong.edu', '2', '2019-05-18', '0', '0');
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21500172@handong.edu', '2', '2019-05-18', '0', '0');
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21500172@handong.edu', '5', '2019-05-18', '0', '0');
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21600301@handong.edu', '3', '2019-05-18', '0', '0');
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21500771@handong.edu', '4', '2019-05-18', '0', '0');
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21600301@handong.edu', '9', '2019-05-15', '0', '0');
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21500172@handong.edu', '1', '2019-05-15', '0', '0');
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21500771@handong.edu', '1', '2019-05-15', '0', '0');
+INSERT INTO `app`.`download_apps` (`email_id`, `app_id`, `date`, `paid_amount`, `is_deleted`) VALUES ('21600301@handong.edu', '1', '2019-05-15', '0', '0');
