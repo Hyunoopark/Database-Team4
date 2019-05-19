20f00e4859c46b8f0c5c09407e63b3563d9da5a4
codesmart3 <49859209+codesmart3@users.noreply.github.com>
2019-05-19 20:42:36 +0900

    Update comment_movies.sql

--- comment_movies.sql
+++ comment_movies.sql
@@ -8,5 +8,10 @@ CREATE TABLE `comments_movies` (
   `reported` INT(11) NULL,
   `user_email` VARCHAR(30) NOT NULL,
   PRIMARY KEY (`comment_id`, `user_email`, `movie_id`));
+               
+ALTER TABLE `app`.`comments_movies` 
+CHANGE COLUMN `num_of_likes` `num_of_likes` INT(11) NULL DEFAULT 0 ,
+CHANGE COLUMN `reported` `reported` INT(11) NULL DEFAULT 0 ;
+
   
   INSERT INTO `comments_movies` (`comment_id`, `movie_id`, `comment_contents`, `score`, `commented_date`, `num_of_likes`, `user_email`) VALUES ('1', '24', '재미없음', '1', '2019-05-21', '1', 'hjkim@gmail.com');
